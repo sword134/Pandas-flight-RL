fcf94c76cb4f14d2d06c0f85d1f627da0064774d
sword134 <Tomas1olav@gmail.com>
2022-09-03 16:38:12 +0200

    Update reward_calculation.py

--- reward_calculation.py
+++ reward_calculation.py
@@ -1,8 +1,8 @@
-from ACUMU.idle_station_optimizer import run_ACUMU
+#from ACUMU.idle_station_optimizer import run_ACUMU #Disabled for stackoverflow to not overcomplicate the problem
 
 def calc_total_reward(df):
-    num_mus = run_ACUMU(filename=df)
-
+    #num_mus = run_ACUMU(filename=df) #Disabled for stackoverflow to not overcomplicate the problem
+    num_mus = len(df) #will obviously always return the same, but the issue for Stackoverflow is not the reward calculation but instead the agent space
     cost_mus = 1000*num_mus
 
     return cost_mus
